c3b01444b659da51a144e00a664b1a4eb5657519
Breyner Mola <breyner.mola.9@gmail.com>
2019-03-27 18:27:27 -0500

    add cors v1

--- api.js
+++ api.js
@@ -28,6 +28,15 @@ app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 app.use('/api', api)
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 //server
 app.listen(port, () => {
   console.log(`API REST running on http://localhost:${port}`)
