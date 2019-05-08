ab0a4972c2938e33d609b0b9ddf180dfb647f5d6
Breyner Mola <breyner.mola.9@gmail.com>
2019-05-08 13:51:03 -0500

    agregando seguridad a rutas

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const userExist = require('../middlewares/users')
 const moment = require('moment')
 
 //users
-api.post('/users', usersCtrl.createUser)
+api.post('/users', auth, usersCtrl.createUser)
 api.get('/users', auth, usersCtrl.readUsers)
 api.put('/users/:userId', auth, userExist, usersCtrl.updateUser)
 api.delete('/users/:userId', auth, userExist, usersCtrl.deleteUser)
@@ -19,20 +19,20 @@ api.get('/users/rol/:rolId', auth, usersCtrl.readUsersByRol)
 api.get('/users/:userId', auth, userExist, usersCtrl.readUserById)
 
 //vinculos
-api.post('/vinculos', vinculosCtrl.createVinculo)
-api.get('/vinculos', vinculosCtrl.readVinculos)
-api.put('/vinculos/:trainerId/:clienteId', vinculosCtrl.updateVinculo)
-api.delete('/vinculos/:trainerId/:clienteId', vinculosCtrl.deleteVinculo)
-api.get('/vinculos/user/:userId', vinculosCtrl.readVinculosOfUser)
-api.get('/vinculos/:trainerId/:clienteId', vinculosCtrl.readVinculoByIds)
+api.post('/vinculos', auth, vinculosCtrl.createVinculo)
+api.get('/vinculos', auth, vinculosCtrl.readVinculos)
+api.put('/vinculos/:trainerId/:clienteId', auth, vinculosCtrl.updateVinculo)
+api.delete('/vinculos/:trainerId/:clienteId', auth, vinculosCtrl.deleteVinculo)
+api.get('/vinculos/user/:userId', auth, vinculosCtrl.readVinculosOfUser)
+api.get('/vinculos/:trainerId/:clienteId', auth, vinculosCtrl.readVinculoByIds)
 
 //citas
-api.post('/citas', citasCtrl.createCita)
-api.get('/citas', citasCtrl.readCitas)
-api.put('/citas/:citaId', citasCtrl.updateCita)
-api.get('/citas/user/:userId', citasCtrl.readCitasOfUser)
-api.delete('/citas/:citaId', citasCtrl.deleteCita)
-api.get('/citas/:citaId', citasCtrl.readCitaById)
+api.post('/citas', auth, citasCtrl.createCita)
+api.get('/citas', auth, citasCtrl.readCitas)
+api.put('/citas/:citaId', auth, citasCtrl.updateCita)
+api.get('/citas/user/:userId', auth, citasCtrl.readCitasOfUser)
+api.delete('/citas/:citaId', auth, citasCtrl.deleteCita)
+api.get('/citas/:citaId', auth, citasCtrl.readCitaById)
 
 module.exports = api
 
